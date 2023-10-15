d0be43f2b7d64c16ed26bd9c6ae5409e3c421daa
Gerardo Maldonado Félix <gmaldonadofelix@gmail.com>
2023-10-14 21:18:31 -0600

    El codigo de BubbleSort se modific para que sea una funcion

--- orden/BubbleSort.py
+++ orden/BubbleSort.py
@@ -2,12 +2,14 @@ import random
 import time
 #lista = [7,3,10,1,9]
 lista = random.sample(range(1,1001),1000)
-#print(lista)
+print(lista)
+def BubbleSort(lista):
+    for i in range(len(lista),0,-1):
+        for j in range(0,(i-1)):
+            if lista[j] > lista[j+1]:
+                lista[j], lista[j+1] = lista[j+1], lista[j]
 inicio = time.time()
-for i in range(len(lista),0,-1):
-    for j in range(0,(i-1)):
-        if lista[j] > lista[j+1]:
-            lista[j], lista[j+1] = lista[j+1], lista[j]
+BubbleSort(lista)
 fin = time.time()
-#print(lista)
+print(lista)
 print(f"Tiempo con BubbleSort: {fin-inicio}")
