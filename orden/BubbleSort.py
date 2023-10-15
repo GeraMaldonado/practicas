d1045c9903a2f6eb68156718da71b2a6aeccf761
Gerardo Maldonado Félix <gmaldonadofelix@gmail.com>
2023-10-14 21:14:18 -0600

    Se agregaron lineas para generar una lista aleatoria y tomar el tiempo

--- orden/BubbleSort.py
+++ orden/BubbleSort.py
@@ -1,7 +1,13 @@
-lista = [8,3,1,19,14]
-print(lista)
+import random
+import time
+#lista = [7,3,10,1,9]
+lista = random.sample(range(1,1001),1000)
+#print(lista)
+inicio = time.time()
 for i in range(len(lista),0,-1):
     for j in range(0,(i-1)):
         if lista[j] > lista[j+1]:
             lista[j], lista[j+1] = lista[j+1], lista[j]
-print(lista)
+fin = time.time()
+#print(lista)
+print(f"Tiempo con BubbleSort: {fin-inicio}")
