cd61dce8e570f3dd31c89c5d2590cd94c9ed073e
Gerardo Maldonado Félix <gmaldonadofelix@gmail.com>
2023-10-14 21:21:35 -0600

    El codigo de InsertionSort se modific para que sea una funcion

--- orden/BubbleSort.py
+++ orden/BubbleSort.py
@@ -2,7 +2,7 @@ import random
 import time
 #lista = [7,3,10,1,9]
 lista = random.sample(range(1,1001),1000)
-print(lista)
+#print(lista)
 def BubbleSort(lista):
     for i in range(len(lista),0,-1):
         for j in range(0,(i-1)):
@@ -11,5 +11,5 @@ def BubbleSort(lista):
 inicio = time.time()
 BubbleSort(lista)
 fin = time.time()
-print(lista)
+#print(lista)
 print(f"Tiempo con BubbleSort: {fin-inicio}")
--- orden/InsertionSort.py
+++ orden/InsertionSort.py
@@ -3,13 +3,15 @@ import time
 #lista = [7,3,10,1,9]
 lista = random.sample(range(1,1001),1000)
 #print(lista)
+def InsertionSort(lista):
+    for i in range(1,len(lista)):
+        for j in range(i,0,-1):
+            if lista[j]<lista[j-1]:
+                lista[j],lista[j-1] = lista[j-1], lista[j]
+            else:
+                break
 inicio = time.time()
-for i in range(1,len(lista)):
-    for j in range(i,0,-1):
-        if lista[j]<lista[j-1]:
-            lista[j],lista[j-1] = lista[j-1], lista[j]
-        else:
-            break
+InsertionSort(lista)
 fin = time.time()
 #print(lista)
 print(f"Tiempo con InsertionSort: {fin-inicio}")
